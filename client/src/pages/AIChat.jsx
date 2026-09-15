ce65bcc8f6ab18f00a7395bf6a6a8e4b09b56b37
SujiGayatri <sujigayatrikadiyala@gmail.com>
2026-09-15 21:30:51 +0530

    Fix RAG chatbot Axios response handling

--- client/src/pages/AIChat.jsx
+++ client/src/pages/AIChat.jsx
@@ -40,7 +40,7 @@ export default function AIChat() {
     question: userText,
   });
 
-      const data = res.json();
+      const data = res.data();
 
       setMessages((prev) => [
         ...prev,
